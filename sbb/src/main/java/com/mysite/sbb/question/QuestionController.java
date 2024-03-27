12705cbee8ecb09002e4e7d3b7848ffcf01d65fe
김지연 <kdelay20@gmail.com>
2024-03-27 15:09:46 +0900

    feat : 검색 기능

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -34,11 +34,13 @@ public class QuestionController {
 	private final UserService userService;
 	
 	@GetMapping("/list")
-	public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) { // Model 객체는 자바 클래스와 템플릿 간의 연결 고리 역할을 한다.
+	public String list(Model model, @RequestParam(value="page", defaultValue="0") int page // Model 객체는 자바 클래스와 템플릿 간의 연결 고리 역할을 한다.
+			, @RequestParam(value = "kw", defaultValue = "") String kw) { 
 		// Model 객체에 값을 담아 두면 템플릿에서 그 값을 사용할 수 있다.
 		// 컨트롤러의 메서드에 매개변수로 지정하기만 하면 스프링 부트가 자동으로 Model 객체를 생성한다.
-		Page<Question> paging = this.questionService.getList(page);
+		Page<Question> paging = this.questionService.getList(page, kw);
 		model.addAttribute("paging", paging);
+		model.addAttribute("kw",kw);
 		return "question_list";
 	}
 	
--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 // 데이터 저장, 조회, 수정, 삭제를 할 수 있도록 도와주는 인터페이스. 테이블에 접근한다.
 // 이와 같이 데이터를 관리하려면 데이터베이스와 연동하는 JPA Repository가 필요하다.
@@ -17,4 +20,21 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{ //
 	List<Question> findBySubjectLike(String subject);
 	// 페이징 기능 추가
 	Page<Question> findAll(Pageable pageable);
+	// Specification을 통해 질문 검색하기 위한 메서드
+	Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+	
+	// Specification 대신 쿼리를 직접 작성하여 검색 기능을 구현할 수 있다.
+	@Query("select "
+			+ "distinct q "
+			+ "from Question q "
+			+ "left outer join SiteUser u1 on q.author=u1 "
+			+ "left outer join Answer a on a.question=q "
+			+ "left outer join SiteUser u2 on a.author=u2 "
+			+ "where "
+			+ "	q.subject like %:kw% "
+			+ "	or q.content like %:kw% "
+			+ "	or u1.username like %:kw% "
+			+ "	or a.content like %:kw% "
+			+ "	or u2.username like %:kw% ")
+	Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
